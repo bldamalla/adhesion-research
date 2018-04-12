cdb2bb7d6f100fefd218d5c1ab319e2b8b2f4af7
bldamalla <bonleifamalla@gmail.com>
2018-04-13 03:09:26 +0800

    Use faster convolution method

--- src/Boundaries.jl
+++ src/Boundaries.jl
@@ -8,12 +8,11 @@ Returns the two-point spatial correlation given an array.
 For symmetric results, always use images with odd number of voxels.
 """
 function convolve_mean(arr::Vector{T}) where T <: AbstractFloat
-  len = length(arr)
-  len % 2 == 1 || throw(ArgumentError("`arr` should have an odd number of elements"))
-
-  sum(0 .<= arr .<= 1) != len && throw(ArgumentError("values should all be between 0 and 1, inclusive"))
-
-  hold = conv(arr, arr[end:-1:1]); len = length(hold); olen = length(arr)
+  olen = length(arr)
+  olen % 2 == 1 || throw(ArgumentError("`arr` should have an odd number of elements"))
+  len = olen*2-1
+  sum(0 .<= arr .<= 1) != olen && throw(ArgumentError("values should all be between 0 and 1, inclusive"))
+  hold = conv(arr, arr[end:-1:1])
   for i=1:div(len, 2)
     hold[i] += hold[i+1+div(len, 2)]
   end
